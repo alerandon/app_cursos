11cdb45b2baa04c1a9595262c464377891944558
alerandon <alejandrorandon@gmail.com>
2020-02-04 15:52:55 -0400

    factories

---
+++ database/factories/UsuariosFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(Model::class, function (Faker $faker) {
+    return [
+        'nombre' => $faker->nombre,
+        'contraseña' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        'correo' => $faker->unique()->safeEmail,
+        'rol' => 'estudiante',
+    ];
+});
